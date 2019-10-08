0aff0ad8b8f60f10fa24bc2f7b409295d87a7b9a
Kaya Raymond Oskal <kay.oskal@gmail.com>
2019-10-08 09:04:17 +0200

    Added 413 errorhandler and changed verson on flask_limiter in requirements

--- app/errors.py
+++ app/errors.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user
 from app import app
 
 @app.errorhandler(404)
@@ -8,3 +9,8 @@ def not_found_error(error):
 @app.errorhandler(500)
 def internal_error(error):
     return render_template('500.html', title='Welcome'), 500
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+    flash('File size to big!')
+    return redirect(url_for('stream', username=current_user.username))
